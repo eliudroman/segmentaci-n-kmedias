d2188ea2ec060faeb80562e8865742dbe4c319c3
eliudroma <eliudromangupe@gmail.com>
2024-04-27 07:50:10 -0600

    kmedias 1.2

--- kmedias.py
+++ kmedias.py
@@ -1,7 +1,18 @@
 import numpy as np
+import random
+
+def kmedias(k, imagen, Error):
+    
+    medias = []
+
+    for punto in range(k):
+        R = random.randint(0, 255)
+        G = random.randint(0, 255)
+        B = random.randint(0, 255)
+
+        medias.append([R,G,B])
+    
+    print(medias)
 
 
-def kmedias(k, Error):
-
-
-    return 1
+kmedias(3,0.5)
